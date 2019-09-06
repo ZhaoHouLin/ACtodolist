f882a68f4aede12a9fc8b06e15a4f89f63c721f4
Raiden <rodes5292@gmail.com>
2019-09-06 09:33:24 +0800

    Add sort feature (ascending) on index page

--- app.js
+++ app.js
@@ -28,11 +28,12 @@ const Todo = require("./models/todo.js");
 // 設定路由
 // Todo 首頁
 app.get("/", (req, res) => {
-  Todo.find((err, todos) => {
-    // 把 Todo model 所有的資料都抓回來
-    if (err) return console.error(err);
-    return res.render("index", { todos: todos }); // 將資料傳給 index 樣板
-  });
+  Todo.find({})
+    .sort({ name: "asc" })
+    .exec((err, todos) => {
+      if (err) return console.error(err);
+      return res.render("index", { todos: todos }); // 將資料傳給 index 樣板
+    });
 });
 // 列出全部 Todo
 app.get("/todos", (req, res) => {
